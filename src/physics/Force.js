50bd73a3f5261e39513ae7eff6f99129e64a5100
A. Matías Quezada <amatiasq@gmail.com>
2012-06-20 17:48:18 +0200

    Added force

---
+++ src/physics/Force.js
@@ -0,0 +1,34 @@
+var IForce = Interface({
+	// Direction
+	getDirection: signature().returns(Number),
+	setDirection: signature(Number).returns(),
+	modifyDirection: signature(Number).returns(Number),
+
+	// Direction in radians
+	getRadians: signature().returns(Number),
+	setRadians: signature(Number).returns(),
+
+	// Strength
+	getStrength: signature().returns(Number),
+	setStrength: signature(Number).returns(),
+	modifyStrength: signature(Number).returns(Number),
+
+	// Vector
+	getVector: signature().returns(IVector),
+
+	// Operators
+	equals: signature('IForce').returns(Boolean),
+	clone: signature().returns('IForce'),
+	merge: signature('IForce').chain()
+});
+
+var Force = IForce(Base.extend({
+	direction: null,
+	strength: 0,
+
+
+	init: function() {
+		this.direction = Vector.extend({ x: 0, y: 0 }).init();
+
+	}
+}))
